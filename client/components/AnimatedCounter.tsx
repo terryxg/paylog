a64cbab27be21f157b136e24e3c91b6132d04dcd
imooje <savvyplus020@gmail.com>
2024-05-28 19:25:29 +0100

    fix: Can't resolve 'react-CountUp'

--- client/components/AnimatedCounter.tsx
+++ client/components/AnimatedCounter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import CountUp from "react-CountUp";
+import CountUp from "react-countup";
 
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return <CountUp decimal="," prefix="$" duration={1} decimals={2} end={amount} />;
